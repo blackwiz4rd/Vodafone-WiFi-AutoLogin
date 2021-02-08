80a0bcbcc9dabc509519baf700607b5e55cb70b4
blackwiz4rd <blackwiz4rd@gmail.com>
2021-02-08 10:13:08 +0100

    Revert "changed requirements installation"
    This reverts commit cd7b98883526749716a7cec4741d256aaa2b7c00.

--- README.md
+++ README.md
@@ -8,7 +8,7 @@ Versione in italiano - (English version below)
 Installazione
 -----------
 Clonate questa repository `git clone https://github.com/blackwiz4rd/Vodafone-WiFi-AutoLogin`<br />
-I requisiti per eseguire lo script python si trovano in `requests-2.txt` se volete usare `python2`, `requests-3.txt` se volte usare `python3`.<br />
+Requisiti per eseguire lo script python si trovano in `install_pip_requirements.sh`.<br />
 È richiesta l'installazione di python e pip.<br />
 Per scaricare python: `https://www.python.org/downloads/`.<br />
 Per scaricare pip su MacOS: `curl -o ~/Downloads/get-pip.py https://bootstrap.pypa.io/get-pip.py`.<br />
@@ -17,7 +17,8 @@ Per scaricare pip su Windows accedete tramite browser a `https://bootstrap.pypa.
 Per installare pip:  `cd ~/Downloads/` ed eseguite `python get-pip.py`.<br /><br />
 Dopo aver installato python e pip eseguite <br />
 `cd ~/Vodafone-WiFi-AutoLogin`<br />
-`pip install -r requests-2.txt` o `pip install -r requests-3.txt`<br />
+`chmod +x install_pip_requirements.sh`<br />
+`./install_pip_requirements.sh`<br />
 
 Configurazione per utenti
 -----------
@@ -29,57 +30,41 @@ Usate `force=True` solo se ottenete NotConnectedToVodafoneWiFiException durante 
 1. `vodafone.config` Esempio n.1 (Utente Vodafone Italia):
 ```
 [config]
-username = your-account@your-provider.com
-password = your-password
-customer = VF_IT
-force = False
-success_url = http://captive.apple.com/hotspot-detect.html
-timeout = 10
+username=your-account@your-provider.com
+password=your-password
+customer=VF_IT
+force=False
 ```
 2. `vodafone.config` Esempio n.2 (Utente Vodafone Spagna):
 ```
 [config]
-username = your-account@your-provider.com
-password = your-password
-customer = VF_ES
-force = False
-success_url = http://captive.apple.com/hotspot-detect.html
-timeout = 10
+username=your-account@your-provider.com
+password=your-password
+customer=VF_ES
+force=False
 ```
 3. `vodafone.config` Esempio n.3 (Utente Pass Customer):
 ```
 [config]
-username = your-account@your-provider.com
-password = your-password
-customer =
-force = False
-success_url = http://captive.apple.com/hotspot-detect.html
-timeout = 10
+username=your-account@your-provider.com
+password=your-password
+customer=
+force=False
 ```
 4. `vodafone.config` Esempio n.4 (Utente Fon Roaming Partner):
 ```
 [config]
-username = your-account@your-provider.com
-password = your-password
-customer = FON_ROAM
-force = False
-success_url = http://captive.apple.com/hotspot-detect.html
-timeout = 10
+username=your-account@your-provider.com
+password=your-password
+customer=FON_ROAM
+force=False
 ```
 Uso per utenti
 -----
 ```
 cd ~/Vodafone-WiFi-AutoLogin/
-```
-Per utilizzare lo script:
-```
 python vodafone.py
 ```
-Di default, questo script genererà un campo `vodafone_url` in `vodafone.conf`. Se intendete connettervi sempre allo stesso Vodafone-WiFi potete utilizzare la flag `-s` per evitare di generare un nuovo `vodafone_url`:
-```
-python vodafone.py -s
-```
-Potrebbe essere utile anche nel caso in cui la parte di generazione del `vodafone_url` non vada a buon fine (in tal caso inserite il campo manualmente).
 
 ------------
 
@@ -89,18 +74,13 @@ Esempio di inserimento per metter lo script in loop ad ogni minuto 0 di ogni ora
 0 * * * * python /Users/luca/Documents/github/Vodafone-WiFi-AutoLogin/vodafone.py
 ```
 
-Su MacOS disabilitate il captive portal:
-```
-sudo mv '/System/Library/CoreServices/Captive Network Assistant.app' '/System/Library/CoreServices/No Captive Network Assistant.app'
-```
-
 English version - (Italian version above)
 ------------
 
 Installation requirements
 -----------
 Clone this repository `git clone https://github.com/blackwiz4rd/Vodafone-WiFi-AutoLogin`<br />
-The requirements to install the script are available in `requests-2.txt` for python2 and `requests-3.txt` for python3.<br />
+Requirements to install the scrip are available in `install_pip_requirements.sh`.<br />
 It is required to install python and pip.<br />
 To download python: `https://www.python.org/downloads/`.<br />
 To download pip on MacOS: `curl -o ~/Downloads/get-pip.py https://bootstrap.pypa.io/get-pip.py`.<br />
@@ -109,7 +89,8 @@ To download pip on Windows use a web broswer `https://bootstrap.pypa.io/get-pip.
 To install pip:  `cd ~/Downloads/` and execute `python get-pip.py`.<br />
 After installing pip make sure the requirements are satisfied, run<br />
 `cd ~/Vodafone-WiFi-AutoLogin`<br />
-`pip install -r requests-2.txt` or `pip install -r requests-3.txt`<br />
+`chmod +x install_pip_requirements.sh`<br />
+`./install_pip_requirements.sh`<br />
 
 
 How to setup for users
@@ -123,58 +104,41 @@ Configuration examples:<br />
 1. `vodafone.config` Example n.1 (Vodafone Italia user):
 ```
 [config]
-username = your-account@your-provider.com
-password = your-password
-customer = VF_IT
-force = False
-success_url = http://captive.apple.com/hotspot-detect.html
-timeout = 10
+username=your-account@your-provider.com
+password=your-password
+customer=VF_IT
+force=False
 ```
 2. `vodafone.config` Example n.2 (Vodafone Spain user):
 ```
 [config]
-username = your-account@your-provider.com
-password = your-password
-customer = VF_ES
-force = False
-success_url = http://captive.apple.com/hotspot-detect.html
-timeout = 10
+username=your-account@your-provider.com
+password=your-password
+customer=VF_ES
+force=False
 ```
 3. `vodafone.config` Example n.3 (Pass Customer user):
 ```
 [config]
-username = your-account@your-provider.com
-password = your-password
-customer =
-force = False
-success_url = http://captive.apple.com/hotspot-detect.html
-timeout = 10
+username=your-account@your-provider.com
+password=your-password
+customer=
+force=False
 ```
 4. `vodafone.config` Example n.4 (Fon Roaming Partner user):
 ```
 [config]
-username = your-account@your-provider.com
-password = your-password
-customer = FON_ROAM
-force = False
-success_url = http://captive.apple.com/hotspot-detect.html
-timeout = 10
+username=your-account@your-provider.com
+password=your-password
+customer=FON_ROAM
+force=False
 ```
------
 How to run the script for users
 -----
 ```
 cd ~/Vodafone-WiFi-AutoLogin/
-```
-To use the script:
-```
 python vodafone.py
 ```
-This script will generate an option `vodafone_url` in `vodafone.conf`. If you will connect to the same Vodafone-WiFi each time, use `-s` to avoid generating a new `vodafone_url`:
-```
-python vodafone.py -s
-```
-This can be useful if `vodafone_url` does not work properly (add the field manually).
 ------------
 
 An additional curl script was added if you wish to use curl only to login
@@ -189,9 +153,4 @@ Loop the script with `crontab -e` and configure it as you like using the site `h
 Example of usage to loop the script at each minute 0 of every hour in `crontab -e`:
 ```
 0 * * * * python /Users/luca/Documents/github/Vodafone-WiFi-AutoLogin/vodafone.py
-```
-
-On MacOS disable the captive portal:
-```
-sudo mv '/System/Library/CoreServices/Captive Network Assistant.app' '/System/Library/CoreServices/No Captive Network Assistant.app'
 ```
--- vodafone.py
+++ vodafone.py
@@ -3,11 +3,23 @@
 #Vodafone-WiFi
 #Automated login for it.portal.vodafone-wifi.com
 
+#Interface
+#isVodafone()
+#isLogged(history) 
+#parseUrl(welcomeUrl, SUCCESS_URL)
+#getConfig()
+#def getPayload(USERFAKE, PASS, CUSTOMER)
+#def connect(USERNAME, PASSWORD, CUSTOMER, SUCCESS_URL)
+
+#dependencies
+#remember to install with "sudo pip install requests"
 import requests
+#remember to install with "sudo pip install configparser"
 import configparser
+#remember to install with "sudo pip install logging"
 import logging
-import argparse
 from os.path import dirname, abspath, join
+from time import sleep
 # for url parsing
 from sys import version_info
 if version_info[0] < 3:
@@ -27,84 +39,61 @@ class NotConnectedToVodafoneWiFiException(Exception):
 		logging.info('You are not connected to Vodafone-WiFi')
 
 #checks if you are connected to a Vodafone Wi-Fi hotspot
-#TODO: check if there is a better way
-def isVodafone(FORCE, TIMEOUT):
+def isVodafone(FORCE):
 	#a get request on http://192.168.6.1 returns 403 when connected to a Vodafone Wi-Fi
 	#other networks may return the same but it would be rare.
 	if FORCE:
 		return True;
 
 	VODAFONE_IP = ['http://192.168.6.1', 'http://192.168.182.1']
-	isVodafone = False
-
 	for IP in VODAFONE_IP:
 		try:
-			r = requests.get(IP, timeout=TIMEOUT)
-			isVodafone = r.status_code != 403
+			r = requests.get(IP, timeout=10)
+			if r.status_code != 403:
+				raise NotConnectedToVodafoneWiFiException()
 		except requests.ConnectionError as e:
 			logging.debug(e)
+			raise NotConnectedToVodafoneWiFiException()
 
-	if not isVodafone:
-		raise NotConnectedToVodafoneWiFiException()
 	logging.debug('You are connected to Vodafone-WiFi')
-	return isVodafone
+	return True
 
 #checks if login was made or not by looking if redirect has been done
-#TODO: check if it works
 def isLogged(history):
 	if history:
 		raise requests.ConnectionError
 
 	return True
-
+	
 def parseUrl(welcomeUrl, SUCCESS_URL):
 	o = urlparse(welcomeUrl)
 	q = parse_qs(o.query)
 	q['res'] = ['login']
 	q['userurl'] = [SUCCESS_URL]
-	return o._replace(query=urlencode(q, True)).geturl().replace("%3A",":").replace("%2F","/")
+	return o._replace(query=urlencode(q, True)).geturl()
 	#can cause undefined behaviour depending on welcomeUrl
 	# return welcomeUrl[0:47] + 'login' + welcomeUrl[54:172] + '&userurl=' + SUCCESS_URL
-
+	
 #reads from file and splits input parameters
 def getConfig():
 	dict = {}
-	# try:
+	try:
 		#getting configuration parameters from the .conf file
-	c = configparser.RawConfigParser()
-	c.read(join(ROOT_DIR,'vodafone.conf'))
-	dict['username'] = c.get('config', 'username')
-	dict['password'] = c.get('config', 'password')
-	dict['customer'] = c.get('config', 'customer')
-	dict['force'] = c.get('config', 'force')
-	dict['success_url'] = c.get('config', 'success_url')
-	dict['timeout'] = c.get('config', 'timeout')
-	if c.has_option('autogen', 'vodafone_url'):
-		dict['vodafone_url'] = c.get('autogen', 'vodafone_url')
-	# except:
-	# 	logging.debug("Please re-check the configuration file")
-	return dict
-
-# set url
-def setConfig(VODAFONE_URL, SKIP_VODAFONE_URL):
-	c = configparser.RawConfigParser()
-	if not SKIP_VODAFONE_URL:
+		c = configparser.ConfigParser()
 		c.read(join(ROOT_DIR,'vodafone.conf'))
-	if not c.has_section('autogen'):
-		c.add_section('autogen')
-	c.set('autogen', 'vodafone_url', VODAFONE_URL)
-	if not SKIP_VODAFONE_URL:
-		with open(join(ROOT_DIR,'vodafone.conf'), 'w') as configfile:
-			c.write(configfile)
-	else:
-		with open(join(ROOT_DIR,'vodafone.conf'), 'a') as configfile:
-			c.write(configfile)
-
+		dict['username'] = c.get('config', 'username')
+		dict['password'] = c.get('config', 'password')
+		dict['customer'] = c.get('config', 'customer')
+		dict['force'] = c.get('config', 'force')
+	except:
+		logging.debug("Please re-check the configuration file")
+	return dict
+	
 #sets data for post request
 def getPayload(USERFAKE, PASS, CUSTOMER):
 	dict = {}
 	dict['chooseCountry'] = CUSTOMER + '%2F'
-	if CUSTOMER == 'VF_IT' or 'VF_ES':
+	if CUSTOMER == 'VF_IT' or 'VF_ES':	
 		dict['userFake'] = USERFAKE
 	else:
 		dict['userFake2'] = USERFAKE
@@ -113,75 +102,71 @@ def getPayload(USERFAKE, PASS, CUSTOMER):
 	dict['rememberMe'] = 'true'
 	dict['_rememberMe'] = 'on'
 
-	return dict
-
-def getVodafoneUrl(SUCCESS_URL, TIMEOUT):
-	hotspotUrl = ''
-	logged = False
-
-	try:
-		r = requests.get(SUCCESS_URL, timeout=TIMEOUT)
-
-		hotspotUrl = r.url
-		logged = isLogged(r.history)
-	except requests.exceptions.Timeout as e:
-		logging.debug(e)
-		logging.debug('Retrying get request because of Timeout')
-	except requests.ConnectionError as e:
-		logging.debug(e)
-		logging.debug('Retrying get request because of ConnectionError')
-
-	return hotspotUrl, logged
+	return dict		
 
 #connects to the network if there's a vodafone captive portal
-def connect(FORCE, USERNAME, PASSWORD, CUSTOMER, SUCCESS_URL, VODAFONE_URL, TIMEOUT):
+def connect(FORCE, USERNAME, PASSWORD, CUSTOMER, SUCCESS_URL):
+	#later assigned
+	hotspotUrl = ''	
+	vodafone = False
 	logged = False
 
-	logging.info('Trying to login...')
+	vodafone = isVodafone(FORCE)
 
-	#generates login url from welcome url
-	#can cause undefined behaviour if captive portal structure is changed
-	parsedUrl = parseUrl(VODAFONE_URL, SUCCESS_URL)
+	#if get request isn't sending response retry in 10 seconds
+	while not hotspotUrl and vodafone:
+		try:
+			r = requests.get(SUCCESS_URL, timeout=10)
 
-	logging.info('Login parsed url:\n ' + parsedUrl)
-	logging.info('Making the payload')
+			hotspotUrl = r.url
+			logged = isLogged(r.history)
+		except requests.exceptions.Timeout as e:
+			logging.debug(e)
+			logging.debug('Retrying get request because of Timeout')
+			sleep(10)
+		except requests.ConnectionError as e:
+			logging.debug(e)
+			logging.debug('Retrying get request because of ConnectionError')
+			sleep(10)
 
-	#Form Data used to login
-	payload = getPayload(USERNAME, PASSWORD, CUSTOMER)
+		#if not logged try login
+		if hotspotUrl and vodafone and not logged:
+			logging.info('Trying to login...')
+		
+			#generates login url from welcome url
+			#can cause undefined behaviour if captive portal structure is changed
+			parsedUrl = parseUrl(hotspotUrl, SUCCESS_URL)
 
-	#may return ConnectionError if login is successful because then it re-establishes connection
-	try:
-		r = requests.post(parsedUrl, data=payload, timeout=TIMEOUT)
+			logging.info('Login parsed url:\n ' + parsedUrl)
+			logging.info('Making the payload')
 
-		logged = isLogged(not r.history)
-	except requests.exceptions.Timeout as e:
-		logging.debug(e)
-	except requests.ConnectionError as e:
-		logging.debug(e)
+			#Form Data used to login
+			payload = getPayload(USERNAME, PASSWORD, CUSTOMER)
+	
+			#may return ConnectionError if login is successful because then it re-establishes connection
+			try:
+				r = requests.post(parsedUrl, data=payload, timeout=20)
 
-	if logged:
-		logging.info('>>> Logged!')
-	else:
-		logging.debug('Login failed, review your username and password in vodafone.conf')
-	return logged
+				logged = isLogged(not r.history)
+			except requests.exceptions.Timeout as e:
+				logging.debug(e)
+			except requests.ConnectionError as e:
+				logging.debug(e)
+		
+		if logged:
+			logging.info('>>> Logged!')
+		else:
+			logging.debug('Login failed, review your username and password in voadafone.conf')
 
 def main():
 	#Logging item
-	logging.basicConfig(filename=join(ROOT_DIR,'vodafone.log'),format='%(asctime)s %(levelname)s %(message)s',level=logging.DEBUG, filemode='w')
+	logging.basicConfig(filename=join(ROOT_DIR,'vodafone.log'),format='%(asctime)s %(levelname)s %(message)s',level=logging.INFO, filemode='w')
 
 	#Logging header
-	logging.info('############################################################################')
-	logging.info('Report issues: https://github.com/blackwiz4rd/Vodafone-WiFi-AutoLogin/issues')
-	logging.info('############################################################################')
+	logging.info('#################################################################')
+	logging.info('Please, report any error that may occurr at blackwiz4rd@gmail.com')
+	logging.info('#################################################################')
 
-	#Argument parser
-	parser = argparse.ArgumentParser()
-	parser.add_argument('-s', '--skip', action='store_true',
-		help="Skip creating a new vodafone_url in vodafone.conf")
-	args = parser.parse_args()
-	SKIP_VODAFONE_URL = args.skip
-
-	#Configuration
 	config = {}
 	try:
 		logging.info('Reading the configuration file (vodafone.conf)')
@@ -193,30 +178,19 @@ def main():
 	USERNAME = config['username']
 	PASSWORD = config['password']
 	CUSTOMER = config['customer']
-	FORCE = config['force'] == 'True'
-	SUCCESS_URL = config['success_url']
-	TIMEOUT = int(config['timeout'])
-	HAS_VODAFONE_URL = 'vodafone_url' in config
-	if HAS_VODAFONE_URL:
-		VODAFONE_URL = config['vodafone_url']
+	FORCE = config['force']
 
 	logging.info('Username: ' + USERNAME)
+	logging.info('Password: ' + PASSWORD)
 	logging.info('Customer: ' + CUSTOMER)
-	logging.info('Force: ' + str(FORCE))
-	logging.info('Success url: ' + SUCCESS_URL)
-	logging.info('Vodafone url: ' + str(HAS_VODAFONE_URL))
+	logging.info('Force: ' + FORCE)
 
 	#Replace '@' with '%40'
 	USERNAME.replace('@','%40')
 
-	vodafone = isVodafone(FORCE, TIMEOUT)
-	if vodafone:
-		logged = False
-		if not SKIP_VODAFONE_URL or not HAS_VODAFONE_URL:
-			VODAFONE_URL, logged = getVodafoneUrl(SUCCESS_URL, TIMEOUT)
-			setConfig(VODAFONE_URL, SKIP_VODAFONE_URL)
-		if not logged:
-			connect(FORCE, USERNAME, PASSWORD, CUSTOMER, SUCCESS_URL, VODAFONE_URL, TIMEOUT)
+	SUCCESS_URL = 'http://captive.apple.com/hotspot-detect.html'
+	
+	connect(FORCE, USERNAME, PASSWORD, CUSTOMER, SUCCESS_URL)
 
 if __name__ == "__main__":
 	main()
